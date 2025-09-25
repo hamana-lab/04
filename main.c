ed452c71f16e2e818ff079b01662f4fda4edaf1b
hamana-lab <hamana@sookmyung.ac.kr>
2025-09-25 15:58:21 +0900

    week04_4

--- main.c
+++ main.c
@@ -3,15 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    int time, min, sec;
-    printf("input the second :");
-    scanf("%i", &time);
-   	
-   	min = time/60;
-   	sec = time%60;
-   	
-   	printf("the time is %i : %i\n", min, sec);
-   	
+    int year;
+    printf("input the year : ");
+    scanf("%i", &year);
+    
+    printf("is the year %i the leap year? : %i\n", year, 
+    ((year%4==0 && year%100!=0) || year%400 == 0));
+    
    	system("PAUSE");
 
 }
