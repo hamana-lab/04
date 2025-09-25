706c2501e4d013e987f980d78f8650ec2e502d4d
hamana-lab <hamana@sookmyung.ac.kr>
2025-09-25 15:45:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,21 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-    int x,y,a,b,c,d,e;
-    printf("input two integer :");
-    scanf("%i %i", &x, &y);
-    a = x+y;
-    b = x-y;
-    c = x*y;
-    d = x/y;
-    e = x%y;
-    
-    printf("+ result is %i\n",a);
-    printf("- result is %i\n",b);
-    printf("* result is %i\n",c);
-    printf("/ result is %i\n",d);
-    printf("%% result is %i\n",e);
-
-    system("PAUSE");	
+    int time, min, sec;
+    printf("input the second :");
+    scanf("%i", &time);
+   	
+   	min = time/60;
+   	sec = time%60;
+   	
+   	printf("the time is %i : %i\n", min, sec);
+   	
+   	system("PAUSE");
 
 }
